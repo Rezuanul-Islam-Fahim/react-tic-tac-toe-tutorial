db13d0da92d1b1de47fda40b587906d1908ed7bc
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-05 14:03:45 +0600

    Minify board codes

--- src/Board.jsx
+++ src/Board.jsx
@@ -31,21 +31,19 @@ const Board = ({ currentPlay, currentMoves, updateBoard }) => {
     return (
         <div>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <SquareButton value={currentMoves[0]} onClick={() => handleClick(0)} />
-                <SquareButton value={currentMoves[1]} onClick={() => handleClick(1)} />
-                <SquareButton value={currentMoves[2]} onClick={() => handleClick(2)} />
-            </div>
-            <div className="board-row">
-                <SquareButton value={currentMoves[3]} onClick={() => handleClick(3)} />
-                <SquareButton value={currentMoves[4]} onClick={() => handleClick(4)} />
-                <SquareButton value={currentMoves[5]} onClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <SquareButton value={currentMoves[6]} onClick={() => handleClick(6)} />
-                <SquareButton value={currentMoves[7]} onClick={() => handleClick(7)} />
-                <SquareButton value={currentMoves[8]} onClick={() => handleClick(8)} />
-            </div>
+            {Array(3).fill(null).map((_, i) => (
+                <div className="board-row">
+                    {Array(3).fill(null).map((_, j) => {
+                        const index = i * 3 + j;
+
+                        return (
+                            <SquareButton
+                                value={currentMoves[index]}
+                                onClick={() => handleClick(index)} />
+                        );
+                    })}
+                </div>
+            ))}
         </div>
     );
 }
